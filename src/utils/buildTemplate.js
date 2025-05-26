66e04789d3bb336b5a8c5b23e4a73ae8238e3e58
stanley <stanleygomesdasilva@gmail.com>
2025-05-26 17:55:03 -0300

    chore: ajuste para import arquivo html

--- src/utils/buildTemplate.js
+++ src/utils/buildTemplate.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import path from 'path';
 import mustache from 'mustache';
 
 export function renderHtml(params, type, templatePath) {
-  const template = fs.readFileSync(`src/templates/${type}/${templatePath}`, 'utf-8');
+  const templateFullPath = path.join(process.cwd(), 'src', 'templates', type, templatePath);
+  const template = fs.readFileSync(templateFullPath, 'utf-8');
   return mustache.render(template, params);
 }
