0b82609dcb343b3590c27bbc0668b698e3fbb756
stanley <stanleygomesdasilva@gmail.com>
2025-06-21 07:00:31 -0300

    fix: ajustes na inscricao e desinscricao de usuario da lista

--- src/application/usecases/set-subscriber-enabled-use-case.ts
+++ src/application/usecases/set-subscriber-enabled-use-case.ts
@@ -1,19 +1,36 @@
 import { Subscriber } from '../../domain/entities/subscriber.entity.js';
 import { BusinessError } from '../../domain/errors/BusinessError.js';
 import { ISubscriberRepository } from '../../domain/port/databases/subscriber.repository.js';
+import { UUID } from '../../shared/utils/uuid.js';
 
 export class SetSubscriberEnabledUseCase {
   constructor(private readonly subscriberRepository: ISubscriberRepository) {}
 
   async execute(email: string, enabled: boolean): Promise<Subscriber>  {
-    const subscriber = await this.subscriberRepository.getByEmail(email);
+    let subscriber = await this.subscriberRepository.getByEmail(email);
 
     if (!subscriber) {
-      throw new BusinessError(`Subscriber with email ${email} not found!`);
+      return this.createSubscriber(email, enabled);
     }
 
-    subscriber.updated_at = new Date();
+    return this.updateSubscriber(subscriber, enabled);
+  }
+
+  private createSubscriber(email: string, enabled: boolean): Promise<Subscriber> {
+    const uuid = UUID.random();
+
+    return this.subscriberRepository.create({
+      uuid,
+      email,
+      enabled,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+  }
+
+  private updateSubscriber(subscriber: Subscriber, enabled: boolean): Promise<Subscriber> {
     subscriber.enabled = enabled;
+    subscriber.updated_at = new Date();
 
     return this.subscriberRepository.updateByEmail(subscriber, subscriber.email);
   }
