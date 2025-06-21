188cd1c945812622a671be99df397446f882d57b
stanley <stanleygomesdasilva@gmail.com>
2025-06-21 14:57:24 -0300

    fix: corrigindo as telas de visualizacao dos textos

--- api/breads/[id].ts
+++ api/breads/[id].ts
@@ -3,5 +3,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { breadRoutes } from '../../src/infra/providers/dependencies.js';
 
 export default function handler(req: VercelRequest, res: VercelResponse): void {
-  breadRoutes.getBread(req, res);
+  breadRoutes.renderPageBread(req, res);
 }
---
+++ src/domain/enums/bread-type.enum.ts
@@ -0,0 +1,5 @@
+export enum BreadType {
+  DEVOTIONAL = 'DEVOTIONAL',
+  STUDY = 'STUDY',
+}
+
